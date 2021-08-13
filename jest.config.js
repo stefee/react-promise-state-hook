e1832016375d18b12aed240de2bbd8d1a35d6f11
Stef <git@srilq.email>
2021-08-13 21:43:11 +0100

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: "ts-jest",
-  reporters: ["default", ["jest-junit", {outputName: "junit-unit.xml"}]],
   testEnvironment: "jsdom",
   globals: {
     "ts-jest": {
