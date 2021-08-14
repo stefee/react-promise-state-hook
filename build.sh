bfcc35eba1bad8fd5611a79691b537d819e95ce2
Stef <git@srilq.email>
2021-08-14 12:19:36 +0100

    Include ts definition

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-npm exec --no esbuild -- usePromiseState.ts --platform=neutral --format=cjs --outfile=usePromiseState.js
-npm exec --no esbuild -- usePromiseState.ts --platform=browser --format=cjs --target=es2017 --minify --outfile=usePromiseState.min.js
+npm exec --no tsc
+npm exec --no esbuild -- usePromiseState.ts --platform=neutral --format=cjs --outfile=usePromiseState.dist.js
+npm exec --no esbuild -- usePromiseState.ts --platform=browser --format=cjs --target=es2017 --minify --outfile=usePromiseState.dist.min.js
